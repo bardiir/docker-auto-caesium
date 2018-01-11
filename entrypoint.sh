a0afa4be973523d075b016b42547e29fa720c8c5
Alexander Faust <bardiir@gmail.com>
2018-01-11 12:21:25 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,13 +19,25 @@ do
             caesiumclt -q 80 -o "/tmp/caesium/" "${file}" | cat; # suppress segmentation faults from caesium for the time beeing
             if [ ${PIPESTATUS[0]} -eq 0 ]; then
                 oldsize=$(wc -c <"${file}")
-                newsize=$(wc -c <"/tmp/caesium/${file}")
-                if [ $newsize -lt $oldsize ]; then
-                    chown `stat -c "%u:%g" "${file}"` "/tmp/caesium/${file}";
-                    chmod `stat -c "%a" "${file}"` "/tmp/caesium/${file}";
-                    mv "/tmp/caesium/${file}" "${file}";
+                
+                if [ -f "/${file}" ]; then # Workaround for caesiumclt bug: https://github.com/Lymphatus/caesium-clt/issues/26
+                    newsize=$(wc -c <"/${file}")
+                    if [ $newsize -lt $oldsize ]; then
+                        chown `stat -c "%u:%g" "${file}"` "/${file}";
+                        chmod `stat -c "%a" "${file}"` "/${file}";
+                        mv "/${file}" "${file}";
+                    else
+                        echo "Optimized file is not smaller. Skipping";
+                    fi
                 else
-                    echo "Optimized file is not smaller. Skipping";
+                    newsize=$(wc -c <"/tmp/caesium/${file}")
+                    if [ $newsize -lt $oldsize ]; then
+                        chown `stat -c "%u:%g" "${file}"` "/tmp/caesium/${file}";
+                        chmod `stat -c "%a" "${file}"` "/tmp/caesium/${file}";
+                        mv "/tmp/caesium/${file}" "${file}";
+                    else
+                        echo "Optimized file is not smaller. Skipping";
+                    fi
                 fi
             else
                 echo "Optimizing file ${file} failed. Skipping";
