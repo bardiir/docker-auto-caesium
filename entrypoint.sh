eb31b2528b2a7530999f1ecf9c8b17e93e64a9c3
Alexander Faust <bardiir@gmail.com>
2019-10-11 19:38:37 +0200

    Removed Workaround for caesiumclt bug, improved skipfile handling for recursive workloads

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 shopt -s nullglob
 
+if [ ! -d "/tmp/caesium/" ]; then
+    mkdir -p "/tmp/caesium"
+fi
+
 echo "Processing ${1}";
 cd "/caesium/$1";
 for file in *
@@ -9,7 +13,7 @@ do
         /caesiumbin/entrypoint.sh "${1}/${file}";
     else 
         if ( [ ${file: -4} == ".png" ] || [ ${file: -4} == ".jpg" ] ); then
-            if grep -Fxq "${file}" /caesium/.caesiumoptimized
+            if grep -Fxq "${file}" .caesiumoptimized
             then
                 echo "${file} already optimized in previous run. Skipping";
                 continue
@@ -20,30 +24,19 @@ do
             if [ ${PIPESTATUS[0]} -eq 0 ]; then
                 oldsize=$(wc -c <"${file}")
                 
-                if [ -f "/${file}" ]; then # Workaround for caesiumclt bug: https://github.com/Lymphatus/caesium-clt/issues/26
-                    newsize=$(wc -c <"/${file}")
-                    if [ $newsize -lt $oldsize ]; then
-                        chown `stat -c "%u:%g" "${file}"` "/${file}";
-                        chmod `stat -c "%a" "${file}"` "/${file}";
-                        mv "/${file}" "${file}";
-                    else
-                        echo "Optimized file is not smaller. Skipping";
-                    fi
+                newsize=$(wc -c <"/tmp/caesium/${file}")
+                if [ $newsize -lt $oldsize ]; then
+                    chown `stat -c "%u:%g" "${file}"` "/tmp/caesium/${file}";
+                    chmod `stat -c "%a" "${file}"` "/tmp/caesium/${file}";
+                    mv "/tmp/caesium/${file}" "${file}";
                 else
-                    newsize=$(wc -c <"/tmp/caesium/${file}")
-                    if [ $newsize -lt $oldsize ]; then
-                        chown `stat -c "%u:%g" "${file}"` "/tmp/caesium/${file}";
-                        chmod `stat -c "%a" "${file}"` "/tmp/caesium/${file}";
-                        mv "/tmp/caesium/${file}" "${file}";
-                    else
-                        echo "Optimized file is not smaller. Skipping";
-                    fi
+                    echo "Optimized file is not smaller. Skipping";
                 fi
             else
                 echo "Optimizing file ${file} failed. Skipping";
             fi
             
-            echo "${file}" >> /caesium/.caesiumoptimized
+            echo "${file}" >> .caesiumoptimized
         fi
     fi
 done
